06e435c6567f2a8403103ab0f8178246c981a697
Eric Lariviere <eric.lariviere@gmail.com>
2017-01-18 05:40:25 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express()
+
+app.get('/api/whoami', function (req, res) {
+    console.log("***************");
+    res.status(200).json({ipaddress:req.ip,
+        language: "",
+        software: ""
+    })
+})
+
+app.listen(process.env.PORT || 8080);
