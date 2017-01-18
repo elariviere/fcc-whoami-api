061578c7675f562fe28a04f6768573eb2eadf253
Eric Lariviere <eric.lariviere@gmail.com>
2017-01-18 05:58:59 +0000

    Log request headers temporarily

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express()
 
 app.get('/api/whoami', function (req, res) {
-    console.log("***************");
+    console.log(req.headers);
     res.status(200).json({ipaddress:req.ip,
         language: "",
         software: ""
